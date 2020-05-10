220e8ad33570b6767e75e501bba21e0f7ce28f27
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-05-10 04:03:50 +0200

    💚 Use ERPNext ext as test base
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/11.1/debian-slim/test/docker_test.sh
+++ images/11.1/debian-slim/test/docker_test.sh
@@ -63,14 +63,6 @@ echo 'Docker tests successful'
 
 FRAPPE_APP_TO_TEST=
 
-echo "Preparing Frappe application '${FRAPPE_APP_TO_TEST}' tests..."
-
-bench set-config allow_tests true
-
-bench doctor
-bench enable-scheduler
-bench doctor
-
 ################################################################################
 # TODO Frappe Unit tests
 # https://frappe.io/docs/user/en/guides/automated-testing/unit-testing
@@ -81,6 +73,14 @@ FRAPPE_APP_UNIT_TEST_PROFILE="$(pwd)/sites/.${FRAPPE_APP_TO_TEST}_unit_tests.pro
 #bench run-tests --help
 
 if [ -n "${FRAPPE_APP_TO_TEST}" ]; then
+
+    echo "Preparing Frappe application '${FRAPPE_APP_TO_TEST}' tests..."
+
+    bench set-config allow_tests true
+
+    bench doctor
+    bench enable-scheduler
+    bench doctor
 
     echo "Executing Unit Tests of '${FRAPPE_APP_TO_TEST}' app..."
     if [ "${TEST_VERSION}" = "10" ]; then
